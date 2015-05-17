12beb2c1ca96fa64a36c25f28f854d602eed8c24
Parafeu <alexdu742008@gmail.com>
2015-05-17 15:28:40 +0200

    Main files

---
+++ src/fr/thejulix/kitpvp/MySQL.java
@@ -0,0 +1,28 @@
+package fr.thejulix.kitpvp;
+
+public class MySQL {
+	
+	public static String url() {
+		return Main.getInstance().getConfig().getString("url");
+	}
+	
+	public static String user() {
+		return Main.getInstance().getConfig().getString("user");
+	}
+
+	public static String password() {
+		return Main.getInstance().getConfig().getString("password");
+	}
+
+	public static String bdd() {
+		return Main.getInstance().getConfig().getString("bdd");
+	}
+	
+	public static void setConfig() {
+		Main.getInstance().getConfig().set("mysql.url", "local");
+		Main.getInstance().getConfig().set("mysql.user", "root");
+		Main.getInstance().getConfig().set("mysql.password", "Mot_De_Passe");
+		Main.getInstance().getConfig().set("mysql.bdd", "KitPvP");
+	}
+
+}
