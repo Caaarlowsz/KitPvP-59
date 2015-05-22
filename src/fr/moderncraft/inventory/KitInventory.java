1af8dc7ab56e59a55af14dc3182a2bc7bb8d2279
Alexandre Muhl <alexdu742008@gmail.com>
2015-05-22 18:38:43 +0200

    kit

---
+++ src/fr/moderncraft/inventory/KitInventory.java
@@ -0,0 +1,32 @@
+package fr.moderncraft.inventory;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+
+import fr.moderncraft.main.Main;
+
+public class KitInventory implements Listener{
+	
+	private Inventory inventory;
+	private Main main;
+	
+	public KitInventory(Main main){
+		super();
+		this.main = main;
+		this.inventory = Bukkit.createInventory(null, 54, "Choisissez votre kit");
+	}
+	
+	private void setKits(){
+		for (Kit kit : main.getConfiguration().getConfigKit().getKitList()) {
+			this.inventory.setItem(kit.getInvSlot(), kit.getLogoItem());
+		}
+	}
+
+}
